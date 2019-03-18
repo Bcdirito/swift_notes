ee39a69a2d8c4b2a7af29f49c44bd1af0a6ae68b
Brian DiRito <bdirito91@gmail.com>
2019-03-18 18:08:04 -0400

    added notes to stringPlayground

---
+++ stringPlayground.swift
@@ -0,0 +1,21 @@
+print("Hello, world!")
+
+// As opposed to JavaScript, var and let do not both allow you to re-declare values
+
+// will not throw error
+var myFavoriteAnimal = "nudibranch"
+myFavoriteAnimal = "dog"
+
+// will through error if I uncomment line 11
+let encouragement = "You can do it!"
+// encouragement = "You got this!"
+
+// How to create a string literal
+let theTruth = "Money can't buy me love."
+
+// One way to initialize an empty string
+var characterPoorString = ""
+
+// Another way
+// Similiar to OO class
+let stringWithPotential = String()
