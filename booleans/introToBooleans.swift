5cf945299f59b292c5b4b5f39ab0d1204abf7095
Brian DiRito <bdirito91@gmail.com>
2019-03-18 19:47:18 -0400

    created introToBooleans

---
+++ booleans/introToBooleans.swift
@@ -0,0 +1,10 @@
+// Intro to Booleans
+
+// the == operator
+// same as ruby
+// == evaluates if a statement is true or false
+// the following evaluates to true
+var description = "the better looking Parkes brother"
+var jarrod = description
+print(jarrod == description)
+
