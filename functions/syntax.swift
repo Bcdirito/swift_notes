737fd235bbbedb786e151f975a6c91f3b8306662
Brian DiRito <bdirito91@gmail.com>
2019-03-18 21:04:34 -0400

    function syntax part 1 done

---
+++ functions/syntax.swift
@@ -0,0 +1,21 @@
+// Function Syntax
+
+import Foundation
+
+// syntax is as follows:
+
+// func nameOfFunction() {
+    // body of function
+// }
+
+// make sure to name functions based off of what they do
+// the name of a function is sometimes referred to as the Function Signature
+
+// returns "Hello!"
+// line 20 actually calls the function
+func sayHello() {
+    print("Hello!")
+}
+
+sayHello()
+
