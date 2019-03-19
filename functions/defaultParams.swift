e9b9509f6b4bf59fb3ec243a8d24f6e512ff4fb9
Brian DiRito <bdirito91@gmail.com>
2019-03-18 21:35:43 -0400

    did defaultParams

---
+++ functions/defaultParams.swift
@@ -0,0 +1,12 @@
+// Default Parameters
+
+import Foundation
+
+// if you find yourself repeating an input value, you can set a default value
+// example
+
+func calculatePriceForMealWithTip(priceOfMeal: Double, tipPercentage: Double = 0.15) -> Double {
+    return priceOfMeal + (priceOfMeal * tipPercentage)
+}
+
+// adding the = sign and a value after "tipPercentage: Double" set a default value
