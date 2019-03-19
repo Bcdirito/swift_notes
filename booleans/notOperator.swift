e99026b2bbe04e19c143a0be8d15d000353a4ae1
Brian DiRito <bdirito91@gmail.com>
2019-03-18 20:10:01 -0400

    notOperator finished

--- booleans/notOperator.swift
+++ booleans/notOperator.swift
@@ -1,1 +1,19 @@
 // The Not Operator -> !
+
+import Foundation
+
+// Same as in Ruby or JavaScript
+// Same as Bang Operator
+// ! can be used to mean false
+
+// returns true
+var finishedHomework = false
+var schoolTomorrow = true
+var notAllowedToPlayVideoGames = !finishedHomework && schoolTomorrow
+print(notAllowedToPlayVideoGames)
+
+// returns true because we used ||
+var hungry = false
+var isPie = true
+var shouldEat = hungry || isPie
+print(shouldEat)
